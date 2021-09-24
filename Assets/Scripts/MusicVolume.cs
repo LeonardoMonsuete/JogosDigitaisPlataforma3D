d59ef82ef14cfc02dc79aa9d605917b5925d69c2
Kleiton Barone Semolini <kleitonpolar6@gmail.com>
2021-09-24 03:25:39 -0300

    Adicionado Menu Principal

---
+++ Assets/Scripts/MusicVolume.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicVolume : MonoBehaviour
+{
+    public AudioSource AudioSource;
+
+    private float musicVolume = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioSource.volume = musicVolume;
+    }
+
+    public void updateVolume(float volume)
+    {
+        musicVolume = volume;
+    }
+}
