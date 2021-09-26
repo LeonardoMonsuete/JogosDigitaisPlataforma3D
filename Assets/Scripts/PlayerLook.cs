c59d4a87e83ebb34fc571f095422bc1252161ff5
LeonardoMonsuete <leonardo.monsuete@outlook.com>
2021-09-26 19:07:30 -0300

    alterando cena jogo e prefab animação tiro e idle

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -47,14 +47,14 @@ public class PlayerLook : MonoBehaviour
 
 
 
-        ////float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        //float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        // //float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        // float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
         
-        //xRotation -= mouseY;
+        // xRotation -= mouseY;
         
-        //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        //transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        ////transform.Rotate(Vector3.up * mouseX);
+        // xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        // transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        // //transform.Rotate(Vector3.up * mouseX);
 
 
 
